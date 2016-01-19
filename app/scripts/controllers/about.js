63f455d75ff04d17eeb849a89d2342f8d5b3169b
Kit Hensel <kithensel@gmail.com>
2016-01-18 16:15:57 -0800

    setup yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name textLearningApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the textLearningApp
+ */
+angular.module('textLearningApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
